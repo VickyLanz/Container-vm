25d4065bcf6f750e009670191ff0d337f6304861
VIGNESH G <vigesh.sekar97@gmail.com>
2022-01-28 18:52:43 +0530

    initial commit

---
+++ Simple container VM/main.tf
@@ -0,0 +1,85 @@
+provider "google" {
+  project = var.project_id
+  zone= var.zone
+  region = var.region
+}
+
+
+locals {
+  instance_name = format("%s-%s", var.instance_name, substr(md5(module.gce-container.container.image), 0, 8))
+}
+
+module "gce-container" {
+  source = "github.com/terraform-google-modules/terraform-google-container-vm.git"
+
+  cos_image_name = var.cos_image_name
+
+  container = {
+    image = "gcr.io/triple-baton-337806/image-test@sha256:0a7e322c7085a174c52a0e77026e99131a6e262fd307c36636a0873e740bea75"
+
+    env = [
+      {
+        name  = "TEST_VAR"
+        value = "Hello World!"
+      },
+    ]
+
+    volumeMounts = [
+      {
+        mountPath = "/cache"
+        name      = "tempfs-0"
+        readOnly  = false
+      },
+    ]
+  }
+
+  volumes = [
+    {
+      name = "tempfs-0"
+
+      emptyDir = {
+        medium = "Memory"
+      }
+    },
+  ]
+
+  restart_policy = "Always"
+}
+
+resource "google_compute_instance" "vm" {
+  project      = var.project_id
+  name         = local.instance_name
+  machine_type = "n1-standard-1"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = module.gce-container.source_image
+    }
+  }
+
+  network_interface {
+    subnetwork_project = var.subnetwork_project
+    subnetwork         = var.subnetwork
+    access_config {}
+  }
+
+  tags = ["container-vm-example","http-server","https-server"]
+
+  metadata = {
+    gce-container-declaration = module.gce-container.metadata_value
+    google-logging-enabled    = "true"
+    google-monitoring-enabled = "true"
+  }
+
+  labels = {
+    container-vm = module.gce-container.vm_container_label
+  }
+
+  service_account {
+    email = var.client_email
+    scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
